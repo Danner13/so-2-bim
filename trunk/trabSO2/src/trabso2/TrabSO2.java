ba388dd19452670965b48f433045b7336b278f1a
alexandreunfried@gmail.com <alexandreunfried@gmail.com@e6feb414-87ce-07af-8b85-1c159d57988b>
2014-06-11 14:02:15 +0000

    Lendo arquivo

--- trunk/trabSO2/src/trabso2/TrabSO2.java
+++ trunk/trabSO2/src/trabso2/TrabSO2.java
@@ -1,12 +1,36 @@
 package trabso2;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class TrabSO2 {
-    static String caminho = System.getProperty("user.dir") + "\\src\\";
-    
-    public static void main(String[] args) {
 
+    static String caminho = System.getProperty("user.dir") + "\\src\\traces";
+
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        String entrada = s.nextLine();//Entrar com o nome do arquivo
+        System.out.println(entrada);
+
+        File arquivo = new File(caminho + "\\" + entrada);
+        FileReader filein;
+        if (arquivo.isFile()) {//achou o arquivo
+            filein = new FileReader(arquivo);
+            BufferedReader in = new BufferedReader(filein);
+            
+            int cont=0;
+            
+            while(in.ready()){
+                String linha = in.readLine();//Lê uma linha no arquivo
+                System.out.println(linha);
+                
+                
+            }
+
+        } else {//arquivo não encontrado
+            System.out.println("Arquivo não encontrado");
+
+        }
     }
 
 }
